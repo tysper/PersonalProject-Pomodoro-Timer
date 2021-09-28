5cf73c829c01dbe6423baab99f5d604f2f4128ef
Pedro Nunes <pedropaulo_nunes@outlook.com>
2021-09-28 15:26:20 -0300

    Fixing script

--- scripts.js
+++ scripts.js
@@ -1,122 +1,137 @@
-"use strict"
+"use strict";
 const timeDisplay = document.querySelector(".time-display");
 
-let timer;
-let timerLog = [];
-let countdownLog = [];
-let isCounting = false;
+let counter;
+let lastPaused = [];
+let lastTimerRunning = [];
+
+// Conditions for starting
+let wasPaused = false;
+let isRunning = false;
+let wasReseted = false;
+let fromDefinedBtns = false;
 
 // Time length
-let pomodoro = 1500;
-let shortBreak = 300;
-let longBreak = 600;
+let timeLength = {
+  pomodoro: 1500,
+  shortBreak: 300,
+  longBreak: 600,
+};
 
-
-function nextOnLine(arr, el) {
-  const line = arr.slice();
-  line.pop();
-  line.push(el);
-  return line;
+function timeOnDisplay(secs) {
+  let minutes = Math.floor(secs / 60) + "",
+    seconds = (secs % 60) + "";
+  timeDisplay.textContent = `${minutes.padStart(2, "0")}:${seconds.padStart(
+    2,
+    "0"
+  )}`;
+  return secs;
 }
 
-function toHoursMinutes(secs) {
-  const minutes = Math.floor(secs/60);
-  const seconds = secs%60;
-
-  return [minutes, seconds];
-}
-
-function displayTime(secs) {
-  const [min, seconds] = toHoursMinutes(secs).map(x => (x+"").padStart(2, "0"));
-  timeDisplay.textContent = `${min}:${seconds}`;
-}
+// function updateLog(log, value) {
+//   log.push(value);
+//   return log.slice();
+// }
 
 function startTimer(time) {
-  if (!isCounting) {
-
-    countdownLog = nextOnLine(countdownLog, time);
-    let countdown = time;
-    let currentTime;
-
-    isCounting = true;
-    
-    if (timerLog.length > 0) {
-      currentTime = timerLog[0];
-    } else {
-      currentTime = time;
-    };
-    
-    if (countdownLog.length > 0) {
-      countdown = countdownLog[0];
-    }
-    
-
-    function updateCountdown(varName) {
-      if (currentTime > -1) {
-        displayTime(currentTime);
-        currentTime--;
-        timerLog = nextOnLine(timerLog, currentTime);
-      } else {
-        resetTimer(varName);
-        return "stopped";
-      }
-    }
-    
-    updateCountdown(timer);
-    
-    timer = setInterval(function() {
-      updateCountdown(timer);
-    }, 1000);
-
-    return countdown;
+  let countdown;
+  if (wasReseted) {
+    wasReseted = false;
+    countdown = lastTimerRunning[lastTimerRunning.length - 1];
+  } else if (wasPaused) {
+    wasPaused = false;
+    countdown = lastPaused[lastPaused.length - 1];
+  } else if (isRunning) {
+    console.log("Timer already started!");
+    return false;
   } else {
-    return "countdown already started";
+    countdown = time;
+  }
+
+  if (fromDefinedBtns) {
+    countdown = time;
+  }
+
+  isRunning = true;
+
+  lastPaused.push(timeOnDisplay(countdown));
+  countdown--;
+
+  counter = setInterval(() => {
+    lastPaused.push(timeOnDisplay(countdown));
+    countdown--;
+
+    if (countdown <= -1) {
+      clearInterval(counter);
+    }
+  }, 1000);
+
+  if (!wasPaused) {
+    lastTimerRunning.push(time);
+    return time;
+  } else {
+    return lastTimerRunning[lastTimerRunning.length - 1];
   }
 }
 
-function stopTimer(timerName) {
-  clearInterval(timerName);
-  isCounting = false;
+function pauseCounter(timer) {
+  clearInterval(timer);
+  wasPaused = true;
+  return timer;
 }
 
-function resetTimer(timerName) {
-  timerLog.pop();
-  stopTimer(timerName)
-  displayTime(countdownLog[0]);
+function resetCounter(timer) {
+  clearInterval(timer);
+  wasPaused = false;
+  isRunning = false;
+  wasReseted = true;
+  timeOnDisplay(lastTimerRunning[lastTimerRunning.length - 1]);
 }
 
 // DOM
 const startBtn = document.querySelector(".start-timer-btn");
-const stopBtn = document.querySelector(".pause-timer-btn");
+const pauseBtn = document.querySelector(".pause-timer-btn");
 const resetBtn = document.querySelector(".reset-timer-btn");
 const pomoBtn = document.querySelector(".pomodoro-time");
 const shortBtn = document.querySelector(".short-break-time");
 const longBtn = document.querySelector(".long-break-time");
 
-startBtn.addEventListener("click", _ => {
-  startTimer(pomodoro);
+const openSettingsBtn = document.querySelector(".settings-btn");
+const closeSettingsBtn = document.querySelector(".close-btn");
+const settingsWindow = document.querySelector(".settings-window");
+
+//  Main frame
+startBtn.addEventListener("click", (_) => {
+  fromDefinedBtns = false;
+  startTimer(timeLength.pomodoro);
 });
-stopBtn.addEventListener("click", _ => {
-  stopTimer(timer);
+pauseBtn.addEventListener("click", (_) => {
+  pauseCounter(counter);
 });
-resetBtn.addEventListener("click", _ => {
-  resetTimer(timer);
+resetBtn.addEventListener("click", (_) => {
+  resetCounter(counter);
 });
 
-pomoBtn.addEventListener("click", _ => {
-  countdownLog = nextOnLine(countdownLog, pomodoro);
-  resetTimer(timer);
-  startTimer(pomodoro);
+pomoBtn.addEventListener("click", (_) => {
+  fromDefinedBtns = true;
+  startTimer(timeLength.pomodoro);
 });
 
-shortBtn.addEventListener("click", _ => {
-  countdownLog = nextOnLine(countdownLog, pomodoro);
-  resetTimer(timer);
-  startTimer(shortBreak);
+shortBtn.addEventListener("click", (_) => {
+  fromDefinedBtns = true;
+  startTimer(timeLength.shortBreak);
 });
 
-longBtn.addEventListener("click", _ => {
-  countdownLog = nextOnLine(countdownLog, pomodoro);
-  resetTimer(timer);
-  startTimer(longBreak);
+longBtn.addEventListener("click", (_) => {
+  fromDefinedBtns = true;
+  startTimer(timeLength.longBreak);
+});
+
+//  Settings frame
+openSettingsBtn.addEventListener("click", () => {
+  settingsWindow.classList.toggle("hidden");
+});
+
+closeSettingsBtn.addEventListener("click", () => {
+  settingsWindow.classList.toggle("hidden");
 });
